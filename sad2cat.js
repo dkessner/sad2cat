40bec26a51766186039f6513bc44c749b3390df4
Darren Kessner <Darren.Kessner@marlborough.org>
2022-02-26 13:40:55 -0800

    work on emojis

--- sad2cat.js
+++ sad2cat.js
@@ -7,6 +7,8 @@ let capture;
 let tracker;
 let classifier;
 
+let emojis = {};
+
 let w = 640; 
 let h = 480;
 
@@ -25,6 +27,11 @@ function setup()
     tracker.start(capture.elt);
 
     initializeEmotion();
+
+    emojis.happy = loadImage("pix/emoji_happy.png");
+    emojis.sad = loadImage("pix/emoji_sad.png");
+    emojis.surprised = loadImage("pix/emoji_surprised.png");
+    emojis.angry = loadImage("pix/emoji_angry.png");
 }
 
 
@@ -46,33 +53,6 @@ function initializeEmotion()
 }
 
 
-function drawBoundingBox(positions)
-{
-    if (!positions) return;
-
-    let minX = width;
-    let maxX = 0;
-    let minY = height;
-    let maxY = 0;
-
-    for (let i=0; i<positions.length; i++)
-    { 
-        let x = positions[i][0];
-        let y = positions[i][1];
-
-        if (x<minX) minX = x;
-        if (x>maxX) maxX = x;
-        if (y<minY) minY = y;
-        if (y>maxY) maxY = y;
-    }
-
-    stroke(0, 255, 0);
-    noFill();
-    rectMode(CORNERS);
-    rect(minX, minY, maxX, maxY);
-}
-
-
 function draw() 
 {
     background(0);
@@ -86,38 +66,79 @@ function draw()
     let cp = tracker.getCurrentParameters();
     let emotionArray = classifier.meanPredict(cp);
     drawEmotionText(emotionArray);
+
+    image(emojis.happy, 0, height/2);
+    image(emojis.sad, 150, height/2);
+    image(emojis.surprised, 300, height/2);
+    image(emojis.angry, 450, height/2);
 }
 
 
 function drawEmotionText(emotionArray)
 {
-    if (!emotionArray) return;
+    // black box
 
     noStroke();
     fill(0);
     rectMode(CORNER);
     rect(width-125, 0, 150, 150);
+
+    // emotion values
+
+    if (!emotionArray) return;
+
     fill(255);
+
     let x = width-100;
     let y = 0;
 
-    let bestEmotion = "";
-    let bestValue = 0;
+    for (let em of emotionArray)
+        text(em.emotion + " " + em.value.toFixed(2), x, y+=25);
 
-    for (let i=0; i<4; i++)
-    {
-        let em = emotionArray[i];
-        let valueFixed = em.value.toFixed(2);
-        text(em.emotion + " " + valueFixed, x, y+=25);
+    // best emotion
 
-        if (em.value > bestValue)
-        {
-            bestEmotion = em.emotion;
-            bestValue = em.value;
-        }
-    }
+    let bestEmotion = getBestEmotion(emotionArray);
 
+    fill(255, 255, 0);
     text(bestEmotion, x, y+=25);
 }
 
+
+function getBestEmotion(emotionArray)
+{
+    let best = {emotion: "", value: 0};
+
+    for (let em of emotionArray)
+        if (em.value > best.value)
+            best = em;
+
+    return best.emotion; 
+}
+
+
+function drawBoundingBox(positions)
+{
+    if (!positions) return;
+
+    let minX = width;
+    let maxX = 0;
+    let minY = height;
+    let maxY = 0;
+
+    for (let position of positions)
+    { 
+        let x = position[0];
+        let y = position[1];
+
+        if (x<minX) minX = x;
+        if (x>maxX) maxX = x;
+        if (y<minY) minY = y;
+        if (y>maxY) maxY = y;
+    }
+
+    stroke(0, 255, 0);
+    noFill();
+    rectMode(CORNERS);
+    rect(minX, minY, maxX, maxY);
+}
 
