74dacab67e919aa63e430c3902b3d4a75d66459a
dkessner laptop <Darren.Kessner@marlborough.org>
2022-02-27 12:56:59 -0800

    testing desktop mode for iphone

--- sad2cat.js
+++ sad2cat.js
@@ -299,7 +299,8 @@ function deviceType()
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
         return "tablet";
     }
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    //else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    else if (/Mobile|Android|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
         return "mobile";
     }
     return "desktop";
